5b3b5f487706acfc566c3e496352e20c390fe7e4
EAL <elias@takefive.dk>
2025-09-01 22:36:18 +0200

    initial commit

---
+++ week1.py
@@ -0,0 +1,6 @@
+from skimage import color, io, measure, img_as_ubyte
+from skimage.measure import profile_line
+from skimage.transform import rescale, resize
+import matplotlib.pyplot as plt
+import numpy as np
+import pydicom as dicom
